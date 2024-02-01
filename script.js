2ecb7c102b2bf95900baa03563d69a13d2be967f
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-01 22:17:29 +0630

    add counter principle in javascript

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const box = document.querySelector(".threeD");
-
+const counter = document.getElementById("counter");
+var start = 1;
+/*
 box.addEventListener("mousemove", (event) => {
   const mouseX = event.pageX;
   const mouseY = event.pageY;
   console.log(mouseX, mouseY);
 });
+*/
+
+function counting() {
+  if (start < 100) {
+    counter.innerHTML = start;
+    start += 1;
+  } else {
+    clearInterval();
+  }
+}
+setInterval(counting, 20);
+/*
+setInterval(function () {
+  counter.innerHTML += "Hello";
+}, 2000);
+*/
